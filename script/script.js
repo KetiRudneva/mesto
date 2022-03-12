6488731c6ea81c6ebd7231f0e3aa687738112021
Ekaterina <rudnevaketi@gmail.com>
2022-03-12 15:16:56 +0300

    add edit form

--- script/script.js
+++ script/script.js
@@ -1,2 +1,16 @@
-let content = document.querySelector(".main");
-let editButton = content.querySelector(".profile__edit-button");
+let editButton = document.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+let closePopup = document.querySelector(".popup__close-button");
+
+function popupOpen() {
+  popup.classList.add("popup_opened");
+  console.log("found!");
+}
+
+function popupClose() {
+  popup.classList.remove("popup_opened");
+  console.log("oops!");
+}
+
+editButton.addEventListener("click", popupOpen);
+closePopup.addEventListener("click", popupClose);
