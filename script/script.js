24bee7e75c1989ca1d3b09183d3ecc17bff29921
Ekaterina <rudnevaketi@gmail.com>
2022-03-12 15:19:13 +0300

    remove log

--- script/script.js
+++ script/script.js
@@ -4,12 +4,10 @@ let closePopup = document.querySelector(".popup__close-button");
 
 function popupOpen() {
   popup.classList.add("popup_opened");
-  console.log("found!");
 }
 
 function popupClose() {
   popup.classList.remove("popup_opened");
-  console.log("oops!");
 }
 
 editButton.addEventListener("click", popupOpen);
