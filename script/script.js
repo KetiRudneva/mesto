29cc792a61cbf137488ecb6150de46007f9d290c
Ekaterina <rudnevaketi@gmail.com>
2022-03-12 16:06:36 +0300

    add edit profile

--- script/script.js
+++ script/script.js
@@ -1,14 +1,30 @@
 let editButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
 let closePopup = document.querySelector(".popup__close-button");
+let submit = document.querySelector(".popup__submit-button");
+
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__text-name");
+let jobInput = document.querySelector(".popup__text-about");
+let profileName = document.querySelector(".profile__name");
+let job = document.querySelector(".profile__profession");
 
 function popupOpen() {
   popup.classList.add("popup_opened");
+  console.log("dddd", formElement);
 }
 
 function popupClose() {
   popup.classList.remove("popup_opened");
 }
 
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  job.textContent = jobInput.value;
+  profileName.textContent = nameInput.value;
+  popupClose();
+}
+
 editButton.addEventListener("click", popupOpen);
 closePopup.addEventListener("click", popupClose);
+formElement.addEventListener("submit", formSubmitHandler);
