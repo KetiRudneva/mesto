7428d158289fb9d08483a4bfd4e348dd545d06cf
Ekaterina <rudnevaketi@gmail.com>
2022-03-28 13:04:39 +0300

    render initial data

--- script/script.js
+++ script/script.js
@@ -1,3 +1,61 @@
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+let cardsContainer = document.querySelector(".elements");
+let cardTemplate = document
+  .querySelector(".card-template")
+  .content.querySelector(".elements__item");
+console.log("1", cardsContainer);
+console.log("2", cardTemplate);
+
+function addNewCard(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardName = cardElement.querySelector(".elements__title");
+  const cardImage = cardElement.querySelector(".elements__image");
+  console.log("3", cardElement);
+  console.log("4", cardName);
+  console.log("5", cardImage);
+
+  cardName.textContent = data.name;
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+
+  return cardElement;
+}
+
+function renderCard(data) {
+  const cardElement = addNewCard(data);
+  cardsContainer.prepend(cardElement);
+}
+
+initialCards.reverse().forEach((cardsContainer) => {
+  renderCard(cardsContainer);
+});
+
 let editButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
 let popupCloseButton = document.querySelector(".popup__close-button");
