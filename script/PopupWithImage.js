d05702b17f59507d791bb93ce318cf56489e6655
Ekaterina <rudnevaketi@gmail.com>
2022-05-19 06:30:54 +0300

    add class PopupWithImage

---
+++ script/PopupWithImage.js
@@ -0,0 +1,16 @@
+import Popup from "./Popup";
+
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._imagePopup = document.querySelector(".popup__fullscreen-image");
+    this._imageDescription = document.querySelector(".popup__description");
+  }
+
+  openPopup(data) {
+    this._imagePopup.alt = data.name;
+    this._imagePopup.src = data.link;
+    this._imageDescription.textContent = data.name;
+    super.openPopup();
+  }
+}
