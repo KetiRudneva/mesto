4073769dcd4cc2b0a3791060fe3d4e7a719c4156
Ekaterina <rudnevaketi@gmail.com>
2022-06-10 13:38:14 +0300

    add constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,14 @@
 // profile input
-const nameInput = document.querySelector(".popup__text-name");
-const jobInput = document.querySelector(".popup__text-job");
+const nameInput = document.querySelector('.popup__text-name');
+const jobInput = document.querySelector('.popup__text-job');
 
 //popups
-const popupEdit = document.querySelector(".popup_edit");
-const popupAdd = document.querySelector(".popup_add");
+const popupEdit = document.querySelector('.popup_edit');
+const popupAdd = document.querySelector('.popup_add');
+const popupDeleteCard = document.querySelector('.popup_delete');
 
 // popups buttons
-const editButton = document.querySelector(".profile__edit-button");
-const addCardButton = document.querySelector(".profile__add-button");
+const editButton = document.querySelector('.profile__edit-button');
+const addCardButton = document.querySelector('.profile__add-button');
 
-export { nameInput, jobInput, popupAdd, popupEdit, editButton, addCardButton };
+export { nameInput, jobInput, popupAdd, popupEdit, popupDeleteCard, editButton, addCardButton };
